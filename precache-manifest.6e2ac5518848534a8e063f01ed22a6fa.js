0a1bffd7f7802afd300cf618312939739e6b7865
N-bred <thehardyboiz1@gmail.com>
2019-09-04 23:18:53 -0500

    Updates

---
+++ precache-manifest.6e2ac5518848534a8e063f01ed22a6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa95c8748f567528eb6984f2db666ba",
+    "url": "/react-music-player/index.html"
+  },
+  {
+    "revision": "399b2d43090e1c1530d1",
+    "url": "/react-music-player/static/css/2.b8d7bdcb.chunk.css"
+  },
+  {
+    "revision": "0fff053726091f57c7b6",
+    "url": "/react-music-player/static/css/main.0e2dba97.chunk.css"
+  },
+  {
+    "revision": "399b2d43090e1c1530d1",
+    "url": "/react-music-player/static/js/2.1851338b.chunk.js"
+  },
+  {
+    "revision": "0fff053726091f57c7b6",
+    "url": "/react-music-player/static/js/main.053b16b4.chunk.js"
+  },
+  {
+    "revision": "27ef9166a1472d6dc4f9",
+    "url": "/react-music-player/static/js/runtime~main.e4672bac.js"
+  }
+]);
